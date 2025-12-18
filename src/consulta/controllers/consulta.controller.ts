c72bcf74ec7072cc3e1b7ce7e64f3b4db3d4e650
Liviadiaxx <liviadiasxavier@hotmail.com>
2025-12-18 13:49:44 -0300

    feat: implementa update e delete da consulta

--- src/consulta/controllers/consulta.controller.ts
+++ src/consulta/controllers/consulta.controller.ts
@@ -1,30 +1,35 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
-import { ConsultaService} from '../services/consulta.service';
-import{Consulta} from '../entities/consulta.entity';
-
+import {
+  Body,
+  Get,
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Put,
+} from '@nestjs/common';
+import { ConsultaService } from '../services/consulta.service';
 
 @Controller('/consulta')
-export class ConsultaController{
- constructor(private readonly consultaService: ConsultaService){}
+export class ConsultaController {
+  constructor(private readonly consultaService: ConsultaService) {}
 
-
-
-@Put (':id')
-
-@HttpCode (HttpStatus.OK)
-update
- (@Param('id', ParseIntPipe) id:number, 
-@Body() body:any,
-) {
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: any,
+  ) {
     return this.consultaService.update(id, body);
-}
-@Delete (':id')
+  }
 
-@HttpCode (HttpStatus.NO_CONTENT)
-delete
- (@Param('id', ParseIntPipe) id:number,
-): Promise<void> {
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<void> {
     return this.consultaService.delete(id);
-}
+  }
 }
 
