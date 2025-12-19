9b1db5644a66f6d84d8d3e63d02a56e0e589c96c
lf-kali <cbjk.kali@gmail.com>
2025-12-19 09:20:54 -0300

    fix: import e export de módulos

--- src/consulta/consulta.module.ts
+++ src/consulta/consulta.module.ts
@@ -4,9 +4,11 @@ import { Consulta } from './entities/consulta.entity';
 import { ConsultaService } from './services/consulta.service';
 import { ConsultaController } from './controllers/consulta.controller';
 import { EspecialidadeModule } from '../especialidade/especialidade.module';
+import { PacienteService } from '../paciente/services/paciente.service';
+import { PacienteModule } from '../paciente/paciente.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Consulta]), EspecialidadeModule],
+  imports: [TypeOrmModule.forFeature([Consulta]), EspecialidadeModule, PacienteModule],
   providers: [ConsultaService],
   controllers: [ConsultaController],
   exports: [],
